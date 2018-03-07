87a1b4e389b0980ad745454d69a32aa35ebe49f6
Adrian Zhao <yzhao333@gmailk.com>
2018-03-06 23:39:50 -0500

    add gitignore

---
+++ README.md
@@ -0,0 +1,1 @@
+# Y_Zhao_Roku
---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+  user : "root",
+  password : "root", // leave this blank for windows machines
+  port: 8889,
+  host: "localhost",
+  database: "getMovies",
+
+  kidsmode : false
+}
+
+module.exports = config;
