ce5aa8e110fca4d2a8cee7814e98c3999cbfaae9
Adrian Zhao <yzhao333@gmailk.com>
2018-03-09 00:30:42 -0500

    stylize

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var toRender = (config.kidsmode) ? 'main_kids' : 'home';
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render(toRender, {
-    title: 'Roku Site',
+    title: 'Roku Flashback Site',
     mainpage: true,
     kidsmode : config.kidsmode
  });
