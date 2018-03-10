485a7f76d1b95c04a4dad74bbf41bc1fa58a0800
Adrian Zhao <yzhao333@gmailk.com>
2018-03-10 00:50:23 -0500

    modification for kids mode

--- routes/actionKids.js
+++ routes/actionKids.js
@@ -4,18 +4,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  connect.query('SELECT * FROM tbl_movies WHERE movie_genre = "animation"', (err, result) => {
+  connect.query('SELECT * FROM tbl_movies WHERE movie_genre = "animation" AND movie_grades = "pnk"', (err, result) => {
     if (err) {
       throw err; console.log(err);
     } else {
       console.log("fire " + result);
 
-      res.render('animation', {
-        title: 'Animation Movie',
+      res.render('actionKids', {
+        title: 'Action Movie',
         videoData : result,
+        kidsmode: true
       });
     }
   });
 });
+
+
 
 module.exports = router;
